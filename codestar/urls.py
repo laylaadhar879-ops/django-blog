cf452e244f6543554feaff4c8fd4c146d04e65b4
laylaadhar879-ops <laylaadhar879@gmail.com>
2026-02-12 14:40:04 +0000

    prepping project for heroku deployment

--- codestar/urls.py
+++ codestar/urls.py
@@ -16,9 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from blog.views import m
+from blog import views as blog_views
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('blog/', my_blog, name='blog'),
-]
+urlpatterns = [ path('admin/', admin.site.urls), path('', blog_views.my_blog, name='home'), ]
