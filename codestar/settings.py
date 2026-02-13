bc34c05b70ac70b0d1ff62dc45d6048d7d25a0e4
laylaadhar879-ops <laylaadhar879@gmail.com>
2026-02-13 13:15:08 +0000

    move gitignore file outside venv folder

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yv-+(gxy0aew90eyc9(!mut-5_rx#*(r#e-2087*y@(93pz_=e'
+SECRET_KEY = os.environ.get("SECRET_ENTRY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [,'.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com', '127,0,0,1']
 
 
 # Application definition
@@ -79,10 +79,10 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 # DATABASES = {
-  #  'default': {
-   #     'ENGINE': 'django.db.backends.sqlite3',
-      #  'NAME': BASE_DIR / 'db.sqlite3',
-  #  }
+# 'default': {
+#      'ENGINE': 'django.db.backends.sqlite3',
+#  'NAME': BASE_DIR / 'db.sqlite3',
+# }
 #}
 
 DATABASES = {
---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL", "postgresql://neondb_owner:npg_AHCbuM0Dth8k@ep-dawn-bonus-agc3gcsw.c-2.eu-central-1.aws.neon.tech/racer_pasta_cloud_646278")
+os.environ.setdefault("SECRET_KEY", "SECRET_ENTRY")
